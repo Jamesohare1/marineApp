0af7036eba896a0709c9ec721aa8980ace527c36
James O'Hare <jimohare@eircom.net>
2021-01-24 01:41:52 +0000

    Updating info modal

--- infoModalContent.R
+++ infoModalContent.R
@@ -1,12 +1,13 @@
 info_Modal_Content <- 
   tagList(
     h3("Display"),
-    p("The app displays the longest distance travelled between two consecutive observations for the ship selected."),
+    p("The app displays the observation when a 'selected ship' sailed the longest distance between two consecutive observations."),
+    p("All summary information shown within the app is based on this longest observation."),
     
     h3("Distance Calculation"),
     p("Distance has been calculated using the haversine formula."),
     p("I have implemented a vectorised version of this calculation. Versions in available packages (e.g. 'geosphere') only accept pairs of points, which would not scale well to the number of observations in the ships dataset."),
-    p("The haversine distance is not entirely accurate for all nautical distance calculations. However, given the number of observations been dealt with, an implementation which 'avoided land' would have been prohibively slow."),
+    p("The haversine distance is not accurate for all nautical distance calculations. However, given the number of observations, an implementation which 'avoided land' when calculating distance would have been prohibitively slow."),
     
     h3("Bearing Calculation"),
     p("Bearing ('angle in a spherical co-ordinate system') has been calculated using the forward-Azimuth calculation."),
